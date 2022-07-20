977bac1a7c60e4c37d710cdbd3124aaa34d3ccbd
jbdevelop3r <justinebanogon14@gmail.com>
2022-07-20 16:16:17 +0800

    buttons

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,79 +1,82 @@
-// https://www.youtube.com/watch?v=AzvpHNkjqsg&t=848s&ab_channel=KennyYipCoding
-
-var board = document.getElementById("board");;
+var board;
 var playerO = "O";
 var playerX = "X";
 var currPlayer = playerO;
 var gameOver = false;
-let changeP = document.getElementById("changePlayer");
-let restartG = document.getElementById("restartGame");
-let homeP = document.getElementById("homePage");
+
+const changeP = document.getElementById("changePlayer");
+const restartG = document.getElementById("restartGame");
+const homeP = document.getElementById("homePage");
 const playwho = document.querySelector("#playerTurn");
 const playGame = document.querySelector("#playGame");
+const draw = document.getElementById("draw");
 const gameOversound = new Audio("audio/click.wav");
 
+box1 = board
 
+board = [
+    [' ', ' ', ' '],
+    [' ', ' ', ' '],
+    [' ', ' ', ' ']
+]
+
+
+moves = [];
 
 window.onload = function() {
     setGame();
 }
 
 
-board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-] 
-
-
 function setGame() {
- 
+
+
     for (let r = 0; r < 3; r++) {
         for (let c = 0; c < 3; c++) {
-            var tile = document.createElement("div");
+            let tile = document.createElement("div");
             tile.id = r.toString() + "-" + c.toString();
             tile.classList.add("tile");
+            tile.innerText = "";
+            tile.addEventListener("click", setTile);
+            document.getElementById("board").appendChild(tile);
+            playwho.textContent = `Player ${currPlayer}'s turn`;
             if (r == 0 || r == 1) {
                 tile.classList.add("horizontal-line");
             }
             if (c == 0 || c == 1) {
                 tile.classList.add("vertical-line");
             } 
-
-            // board.classList.add("hide")
-            tile.innerText = "";
-            tile.addEventListener("click", setTile);
-            document.getElementById("board").appendChild(tile);
-            playwho.textContent = `Player ${currPlayer}'s turn`; 
-            
         }
+        
     }
 }
 
-// function tileClick(event) {
-//     if(gameOver.classlist.contains('visible')) {
-//         return;
-//     }
-// }
 
-// tile.forEach(tile=>tile.addEventListener('click, tileClick'));
+
 
 
 function setTile() {
-   
+    if (gameOver) {
+        return;
+    }
 
     let coords = this.id.split("-");    //ex) "1-2" -> ["1", "2'"]
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
-    
 
     if (board[r][c] != ' ') { 
-        //pag na click na di na pwede iclick
+        //already taken spot
         return;
     }
+
+    if (board[0][0] != null) {
+        
+        console.log(board)
+    } 
+
     
-    board[r][c] = currPlayer; //mark the board
-    this.innerText = currPlayer; //mark the board on html
+    board[r][c] = currPlayer; //pwede iclick 
+    this.innerText = currPlayer; //kung kaninong turn, un ang mag occupy
 
     //change players
     if (currPlayer == playerO) {
@@ -90,8 +93,7 @@ function setTile() {
     if (gameOver) {
         return;
     }
-
-    //check winner
+   
     checkWinner();
 }
 
@@ -100,49 +102,42 @@ function checkWinner() {
     //horizontally, check 3 rows
     for (let r = 0; r < 3; r++) {
         if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != ' ') {
-            //if we found the winning row
-            //apply the winner style to that row
+            
+            //pag nameet ang condition, lagyan ng style ung tiles.
             for (let i = 0; i < 3; i++) {
                 let tile = document.getElementById(r.toString() + "-" + i.toString());
                 tile.classList.add("winner");
-                playwho.textContent = `Player ${currPlayer} won the game!`;
-                restartG.addEventListener('click', restartGame);
+                playwho.textContent = `Player ${currPlayer}'s turn`;
             }
             gameOver = true;
             return;
-            
         }
     }
 
     //vertically, check 3 columns
     for (let c = 0; c < 3; c++) {
         if (board[0][c] == board[1][c] && board[1][c] ==  board[2][c] && board[0][c] != ' ') {
-            //if we found the winning col
-            //apply the winner style to that col
+            
             for (let i = 0; i < 3; i++) {
                 let tile = document.getElementById(i.toString() + "-" + c.toString());                
                 tile.classList.add("winner");
-            
-                restartG.addEventListener('click', restartGame);
             }
             gameOver = true;
             return;
         }
     }
 
-    //diagonally
+    //diagonal 
     if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != ' ') {
         for (let i = 0; i < 3; i++) {
             let tile = document.getElementById(i.toString() + "-" + i.toString());                
             tile.classList.add("winner");
-            
-            restartG.addEventListener('click', restartGame);
         }
         gameOver = true;
         return;
     }
 
-    //anti-diagonally
+    //other diagonal 
     if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[0][2] != ' ') {
         //0-2
         let tile = document.getElementById("0-2");                
@@ -156,28 +151,32 @@ function checkWinner() {
         tile = document.getElementById("2-0");                
         tile.classList.add("winner");
         gameOver = true;
-    
         return;
     }
+
+    // if all tiles are taken = draw
+    if (board[0][0] != '' && board[0][1] != ' ' && board[0][2] != ' ' && board[1][0] != ' ' && board[1][1] != ' ' && board[1][2] != ' ' && board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ') {
+        
+        draw.classList.remove('draw')
+        draw.classList.add('drawShow')
+        playwho.textContent = "NO WINNER";
+        gameOver = true;
+        return;
+    } 
+
+
+
+    if (board[0][0] != ' ' || board[0][1] != ' ' || board[0][2] != ' ' || board[1][0] != ' ' || board[1][1] != ' ' || board[1][2] != ' ' || board[2][0] != ' ' || board[2][1] != ' ' || board[2][2] != ' ') {
+        
+        changeP.classList.add('hide');
+        changeP.classList.remove('changeP')
+        return;
+    } 
+
 }
 
+// 
 
-
-function startPlaying(){
-
- 
-        for (let r = 0; r < 3; r++) {
-            for (let c = 0; c < 3; c++) {
-                tile.addEventListener("click", setTile);
-            
-                playwho.textContent = `Player ${currPlayer}'s turn`;
-                
-            
-        }
-    }
-}
-
-playGame.addEventListener('click', startPlaying);
 
 
 document.getElementById('changePlayer').onclick = function(){
@@ -193,22 +192,15 @@ document.getElementById('changePlayer').onclick = function(){
 
 }
 
+
+
+
 function restartGame(){
     setTimeout(() => {
         location.reload();
-    }, 1000);
-
+    });
 }
 
 restartG.addEventListener('click', restartGame);
 
 
-// function noWinner(){
-//     if(gameOver = true) {
-//         alert('hahah')
-//     }
-// }
-
-// noWinner();
-
-console.log(board)
