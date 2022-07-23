f9ca40820f48abbf17e3f28e5c8bec1e96f0273d
jbdevelop3r <justinebanogon14@gmail.com>
2022-07-23 08:16:36 +0800

    done

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -224,7 +224,7 @@ function checkWinner() {
     }
 
     // if all tiles are taken = draw
-    if (board[0][0] != '' && board[0][1] != ' ' && board[0][2] != ' ' && board[1][0] != ' ' && board[1][1] != ' ' && board[1][2] != ' ' && board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ') {
+    if (board[0][0] != ' ' && board[0][1] != ' ' && board[0][2] != ' ' && board[1][0] != ' ' && board[1][1] != ' ' && board[1][2] != ' ' && board[2][0] != ' ' && board[2][1] != ' ' && board[2][2] != ' ') {
         
         // draw.classList.remove('draw')
         // draw.classList.add('drawShow')
